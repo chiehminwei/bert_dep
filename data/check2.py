987f384ad9973d913983c10fbcf859e74f178918
Chiehmin Wei <cmw025@bucknell.edu>
2019-02-11 16:29:37 +0000

    removed data

--- data/check2.py
+++
@@ -1,6 +0,0 @@
-with open('predictions.txt') as f, open('check') as ff:
-	for i, (l, ll) in enumerate(zip(f, ff)):
-		if l.split('\t')[0] != ll.split('\t')[0] and not l.split('\t')[0].startswith('[UNK]') and l.split('\t')[0] != '"': 
-			print(i)
-			break
-			
