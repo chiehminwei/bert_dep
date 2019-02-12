62e4d729550e20268cdcd67a96911fa420cc50f8
Chiehmin Wei <cmw025@bucknell.edu>
2019-02-12 08:15:19 +0000

    sparse softmax

--- dep_parser.py
+++ dep_parser.py
@@ -141,7 +141,8 @@ class Parser(object):
 
 		# predictions = (batch_size, bucket_size) 
 		predictions = tf.math.argmax(masked_logits, -1)
-		accuracy = tf.metrics.accuracy(targets, predictions, weights=mask_horizontal)
+		targets_for_accuracy = tf.math.argmax(targets, -1)
+		accuracy = tf.metrics.accuracy(targets_for_accuracy, predictions, weights=mask_horizontal)
 		loss = tf.losses.sparse_softmax_cross_entropy(targets, masked_logits, mask_horizontal)
 
 		output = {
