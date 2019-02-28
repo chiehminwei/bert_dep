91ca465039ef06b4c8443dd6fde498d22c8fdc59
Chiehmin Wei <cmw025@bucknell.edu>
2019-02-28 12:29:44 +0000

    fixed biaffine

--- dep_parser.py
+++ dep_parser.py
@@ -3,34 +3,8 @@ import numpy as np
 import tensorflow as tf
 import linalg
 
-class Biaffine(object):
-	def __init__(self, x, y, n_in, n_out=1, bias_x=True, bias_y=True):
-		self.n_in = n_in
-		self.n_out = n_out
-		self.bias_x = bias_x
-		self.bias_y = bias_y
-		batch_size, max_seq_length, embedding_size = modeling.get_shape_list(x, expected_rank=3)
-		self.weight = tf.get_variable("biaffine_weight", 
-									[batch_size, n_out, n_in + bias_x, n_in + bias_y], 
-									dtype=tf.float32,
-									initializer=tf.zeros_initializer)
 
-		if self.bias_x:
-			x = tf.concat([x, tf.ones(tf.stack([batch_size, max_seq_length, 1]))], 2)
-		if self.bias_y:
-			y = tf.concat([y, tf.ones(tf.stack([batch_size, max_seq_length, 1]))], 2)
-		# [batch_size, 1, seq_len, d]
-		x = tf.expand_dims(x, 1)
-		# [batch_size, 1, seq_len, d]
-		y = tf.expand_dims(y, 1)
-		# [batch_size, 1, seq_len, d_1] @ [batch_size, n_out, d_1, d_2] @ [batch_size, 1, d_2, seq_len]
-		# => [batch_size, n_out, seq_len, d_2] @ [batch_size, 1, d_2, seq_len]
-		# => [batch_size, n_out, seq_len, seq_len]
-		s = x @ self.weight @ tf.transpose(y, perm=[0, 1, 3, 2])
-		# remove dim 1 if n_out == 1
-		s = tf.squeeze(s, 1)
 
-		return s
 
 
 class Parser(object):
@@ -53,12 +27,12 @@ class Parser(object):
 		lab_h = self.MLP(inputs, self.label_mlp_size)
 		lab_d = self.MLP(inputs, self.label_mlp_size)
 
-		s_arc = Biaffine(arc_d, arc_h, 
+		s_arc = self.biaffine(arc_d, arc_h, 
 						n_in=self.arc_mlp_size,
 						bias_x=True,
 						bias_y=False)
 
-		lab_attn = Biaffine(lab_d, lab_h, 
+		lab_attn = self.biaffine(lab_d, lab_h, 
 							n_in=self.label_mlp_size,
 							n_out=self.num_rel_labels,
 							bias_x=True,
@@ -112,6 +86,34 @@ class Parser(object):
 					kernel_initializer=tf.orthogonal_initializer())
 		mlp = tf.layers.dropout(mlp, self.mlp_droput_rate, training=self.is_training)			
 		return mlp
+
+	def biaffine(self, x, y, n_in, n_out=1, bias_x=True, bias_y=True):
+		self.n_in = n_in
+		self.n_out = n_out
+		self.bias_x = bias_x
+		self.bias_y = bias_y
+		batch_size, max_seq_length, embedding_size = modeling.get_shape_list(x, expected_rank=3)
+		self.weight = tf.get_variable("biaffine_weight", 
+									[batch_size, n_out, n_in + bias_x, n_in + bias_y], 
+									dtype=tf.float32,
+									initializer=tf.zeros_initializer)
+
+		if self.bias_x:
+			x = tf.concat([x, tf.ones(tf.stack([batch_size, max_seq_length, 1]))], 2)
+		if self.bias_y:
+			y = tf.concat([y, tf.ones(tf.stack([batch_size, max_seq_length, 1]))], 2)
+		# [batch_size, 1, seq_len, d]
+		x = tf.expand_dims(x, 1)
+		# [batch_size, 1, seq_len, d]
+		y = tf.expand_dims(y, 1)
+		# [batch_size, 1, seq_len, d_1] @ [batch_size, n_out, d_1, d_2] @ [batch_size, 1, d_2, seq_len]
+		# => [batch_size, n_out, seq_len, d_2] @ [batch_size, 1, d_2, seq_len]
+		# => [batch_size, n_out, seq_len, seq_len]
+		s = x @ self.weight @ tf.transpose(y, perm=[0, 1, 3, 2])
+		# remove dim 1 if n_out == 1
+		s = tf.squeeze(s, 1)
+
+		return s
 
 	def select_indices(inputs, indices):
 		nd_indices = tf.stack([tf.range(tf.shape(inputs)[0]), indices], axis=1)
